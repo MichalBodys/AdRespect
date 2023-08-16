e8d969b908f9d817d8bbad7dd2feee4e2209075b
MichalBodys <bodysmic@gmail.com>
2023-08-17 01:42:59 +0200

    added sections

--- Js/main.js
+++ Js/main.js
@@ -1,1 +1,17 @@
 import '../style.css'
+// import {posts} from './images'
+const {posts} = require('./images.js');
+
+const wrapper = document.querySelector('.image-wrapper')
+
+function generateMasonGrid(columns, posts){
+    wrapper.innerHTML= '';
+
+    let columnWrappers = {};
+    for (let i = 0; i< columns; i++){
+        columnWrappers[`column${i}`] = []
+    }
+    console.log(columnWrappers)
+}
+
+generateMasonGrid(4, posts)
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,13 @@
 export default {
   content: ["./*.{html,js}"],
   theme: {
+    colors:{
+      'beige': '#DCC1AB',
+      'green': '#1B5B31',
+      'white': '#fff',
+      'grey': '#F5F0EC',
+      'black': '#111111'
+    },
     extend: {},
   },
   plugins: [],
